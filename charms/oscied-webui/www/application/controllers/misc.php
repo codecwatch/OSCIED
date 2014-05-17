b1577304e9f5a15ab7b4e7482a1ed7f7b8c632a0
Axel Angel <axel+git@vneko.ch>
2014-05-18 00:41:32 +0200

    Fix attribute measures access

--- charms/oscied-webui/www/application/controllers/misc.php
+++ charms/oscied-webui/www/application/controllers/misc.php
@@ -90,7 +90,7 @@ class Misc extends MY_Controller
                && !in_array($media->parent->filename, $_GET['source']))
                     continue;
            if (isset($_GET['git_url']) && assert(is_array($_GET['git_url']))
-               && !in_array($media->measures->git_url, $_GET['git_url']))
+               && !in_array($media->metadata->measures->git_url, $_GET['git_url']))
                     continue;
 
            // value (supported metrics)
