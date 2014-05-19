b1f7e1166ba8e873b2571de690b2d8c0a88393be
Axel Angel <axel+git@vneko.ch>
2014-05-19 17:04:37 +0200

    Implement loop over root-medias and git cache,

--- charms/oscied-orchestra/api/cron_enco.py
+++ charms/oscied-orchestra/api/cron_enco.py
@@ -20,15 +20,19 @@ if __name__ == '__main__':
     orchestra = OrchestraAPICore(local_config)
 
     user_id = "12f67500-aa53-4bc8-89e8-2e3da74c14e7" # David Fischer
-    media_in_id = "cdd90479-f8aa-46dd-ba89-de2b1b5488fa" # bus_cif.webm
     send_email = 'false'
     queue = 'transform'
+    ibitrates = [100,200,400,800,1600]
 
+    cloned = {}
+
+    # Only: git profiles, root medias
     profiles = orchestra.get_transform_profiles({'encoder_name': 'from_git'})
-    for profile in profiles:
+    medias = orchestra.get_medias({'parent_id':None})
+    for (profile, ibitrate, media) in [(p,b,m) for p in profiles for b in ibitrates for m in medias]:
 
+        media_in_id = media._id
         profile_id = profile._id
-        input_bitrates = [100,200,400,800,1600]
 
         if profile.title == "libvpx-vp8 git":
             extension = ".webm"
@@ -45,15 +49,18 @@ if __name__ == '__main__':
 
         tmpdir = tempfile.mkdtemp()
         # Use the same git commit for every file and every bitrate we encode for
-        git_commit = check_output('git clone --quiet --depth=1 "{0}" "{1}" && cd "{1}" && git rev-parse HEAD && cd .. && rm -rf "{1}"'
-                                  .format(git_url, tmpdir), shell=True).rstrip()
+        if not git_url in cloned:
+            print("Cloning repo {0}".format(git_url))
+            cloned[git_url] = check_output('git clone --quiet --depth=1 "{0}" "{1}" && cd "{1}" && git rev-parse HEAD && cd .. && rm -rf "{1}"'
+                                      .format(git_url, tmpdir), shell=True).rstrip()
+        git_commit = cloned[git_url]
 
-        for input_bitrate in input_bitrates:
-            title = 'cronjob_%s_%s_%d' % (int(time()), profile.title, input_bitrate)
-            out_filename = title + extension
-            metadata = { 'title': title, 'git_url': git_url, 'git_commit': git_commit,
-                         'build_cmds': build_cmds, 'input_bitrate': input_bitrate }
-            print(metadata)
-            orchestra.launch_transform_task(user_id, media_in_id, profile_id,
-                    out_filename, metadata, send_email, queue,
-                    u'/transform/callback')
+        title = 'cronjob_%s_%s_%d' % (int(time()), profile.title, ibitrate)
+        out_filename = title + extension
+        metadata = { 'title': title, 'git_url': git_url, 'git_commit': git_commit,
+                     'build_cmds': build_cmds, 'input_bitrate': ibitrate }
+        print(["Start job", metadata])
+
+        orchestra.launch_transform_task(user_id, media_in_id, profile_id,
+                out_filename, metadata, send_email, queue,
+                u'/transform/callback')
